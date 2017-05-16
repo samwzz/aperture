ba303f5f73ca58d83d6f28a01a9d10c2a0769718
Sam Wang <sam.zz.wang@gmail.com>
2017-05-16 10:31:06 -0700

    Add store and thunkMiddleware

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as APIUtil from './util/session_api_util';
+import * as APIUtil from './actions/session_actions';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   window.signup = APIUtil.signup;
   window.login = APIUtil.login;
   window.logout = APIUtil.logout;
+  const store = configureStore();
+  window.store = store;
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to FStop</h1>, root);
 });
