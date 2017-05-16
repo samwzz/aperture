d3c73d6c6713284d5fc30e2174830d03779a1427
Sam Wang <sam.zz.wang@gmail.com>
2017-05-16 07:03:05 -0700

    Add UsersController, Modify ApplicationController

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'static_pages#root'
 
-  
+  namespace :api, defaults: { format: json } do
+    resources :users, only: [:create]
+    resource :session, only: [:new, :create, :destroy]
+  end
 end
