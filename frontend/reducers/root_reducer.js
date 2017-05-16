e0bb20b9c0844c84d87ea03588c5749c3376cf4e
Sam Wang <sam.zz.wang@gmail.com>
2017-05-16 08:29:52 -0700

    Add SessionReducer and RootReducer

---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,8 @@
+import { combineReducer } from 'redux';
+import SessionReducer from './session_reducer';
+
+const RootReducer = combineReducer({
+  session: SessionReducer
+});
+
+export default RootReducer;
