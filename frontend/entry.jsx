f61f73027a0606516dabda4cee7635a8f3166b6d
Sam Wang <sam.zz.wang@gmail.com>
2017-05-16 10:49:43 -0700

    Add App and Root components, update entry point

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as APIUtil from './actions/session_actions';
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
-  window.signup = APIUtil.signup;
-  window.login = APIUtil.login;
-  window.logout = APIUtil.logout;
   const store = configureStore();
-  window.store = store;
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to FStop</h1>, root);
+  ReactDOM.render(<Root store={ store }/>, root);
 });
