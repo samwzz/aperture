0648da69a9e97fe966eb2a003f22ad4ef2270ec0
Sam Wang <sam.zz.wang@gmail.com>
2017-05-16 11:35:48 -0700

    Add Greeting components, logout button working

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
+import { login } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
+  window.store = store;
+  window.login = login;
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store }/>, root);
 });
