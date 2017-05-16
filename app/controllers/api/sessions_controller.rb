3b5cb04c08cd47557a13db233d921bbf22e6f4dc
Sam Wang <sam.zz.wang@gmail.com>
2017-05-16 07:09:39 -0700

    Add SessionsController

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,27 @@
 class Api::SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user
+      login(@user)
+      render :show
+    else
+      render json: ["Invalid username/password combination"], status: 401
+    end
+  end
+
+  def destroy
+    if current_user
+      logout
+      render json: {}
+    else
+      render json: ["Nobody signed in"], status: 404
+    end
+  end
 end
