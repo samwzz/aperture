bfdba1b4675baca79c01d85139a660f66dd61397
Sam Wang <sam.zz.wang@gmail.com>
2017-05-16 17:26:28 -0700

    Test modals, create Nav components

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -1,5 +1,30 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
+import Modal from 'react-modal';
+
+const style = {
+  overlay : {
+    position        : 'fixed',
+    top             : 0,
+    left            : 0,
+    right           : 0,
+    bottom          : 0,
+    backgroundColor : 'rgba(255, 255, 255, 0.75)',
+    zIndex          : 10
+  },
+  content : {
+    position        : 'fixed',
+    top             : '100px',
+    left            : '150px',
+    right           : '150px',
+    bottom          : '100px',
+    border          : '1px solid #ccc',
+    padding         : '20px',
+    zIndex          : 11,
+    opacity         : 0,
+    transition      : 'opacity 1s'
+  }
+};
 
 class SessionForm extends React.Component {
   constructor(props) {
@@ -7,9 +32,13 @@ class SessionForm extends React.Component {
     this.state = {
       username: "",
       email: "",
-      password: ""
+      password: "",
+      modalOpen: false
     };
     this.handleSubmit = this.handleSubmit.bind(this);
+    // this.openModal = this.openModal.bind(this);
+    // this.closeModal = this.closeModal.bind(this);
+    // this.afterModalOpen = this.afterModalOpen.bind(this);
   }
 
   componentWillReceiveProps(nextProps) {
@@ -32,9 +61,9 @@ class SessionForm extends React.Component {
 
   navLink() {
     if (this.props.formType === 'login') {
-      return <Link to="/signup">Sign up instead</Link>;
+      return <Link to="/signup">Sign Up</Link>;
     } else {
-      return <Link to="/login">Log in instead</Link>;
+      return <Link to="/login">Log In</Link>;
     }
   }
 
@@ -69,13 +98,43 @@ class SessionForm extends React.Component {
     }
   }
 
+  // openModal() {
+  //   this.setState({ modalOpen: true });
+  // }
+  //
+  // closeModal() {
+  //   this.setState({ modalOpen: false });
+  //   style.content.opacity = 0;
+  // }
+  //
+  // afterModalOpen() {
+  //   style.content.opacity = 100;
+  // }
+  //
+  // render() {
+  //   return(
+  //     <div>
+  //       <button onClick={this.openModal}>Open Me!</button>
+  //
+  //       <Modal
+  //         isOpen={this.state.modalOpen}
+  //         onAfterOpen={this.afterModalOpen}
+  //         onRequestClose={this.closeModal}
+  //         style={style}
+  //         contentLabel="Modal"
+  //         >
+  //         <h2>Im a modal!</h2>
+  //         <p>modal modal modal modal modal</p>
+  //         <p>mooooooooodal!</p>
+  //         <button onClick={this.closeModal}>Close</button>
+  //       </Modal>
+  //     </div>
+  //   );
+  // }
   render() {
     return (
       <div className="login-form-container">
         <form onSubmit={this.handleSubmit} className="login-form-box">
-          Welcome to FStop!
-          <br />
-            Please {this.props.formType} or {this.navLink()}
            {this.renderErrors()}
            <div className="login-form">
              <br/>
--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
+import Modal from 'react-modal';
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;
@@ -12,7 +13,8 @@ document.addEventListener('DOMContentLoaded', () => {
   } else {
     store = configureStore();
   }
-  
+
+  Modal.setAppElement(document.body);
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store }/>, root);
 });
