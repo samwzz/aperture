894931d88c5fd9b2c373f696a67a478e72784500
Sam Wang <sam.zz.wang@gmail.com>
2017-05-18 16:02:45 -0700

    Add photo actions

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import SessionReducer from './session_reducer';
+import PhotoReducer from './photo_reducer';
 
 const RootReducer = combineReducers({
-  session: SessionReducer
+  session: SessionReducer,
+  photos: PhotoReducer
 });
 
 export default RootReducer;
