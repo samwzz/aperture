514d4d28d02222742a64480c5cc992d9ec20a836
Sam Wang <sam.zz.wang@gmail.com>
2017-05-18 14:30:47 -0700

    Add routes for photos

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create]
     resource :session, only: [:new, :create, :destroy]
+    resources :photos, only: [:index, :new, :create, :edit, :update, :destroy]
   end
 end
