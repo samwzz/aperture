8af5c390a0eb837f9297181f2bc09b2c52b89700
Sam Wang <sam.zz.wang@gmail.com>
2017-05-19 22:22:50 -0700

    Working gallery, remove modal from PhotoIndex

--- frontend/components/photo_index/photo_index.jsx
+++ frontend/components/photo_index/photo_index.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import PhotoIndexItem from './photo_index_item';
+import PhotoModal from './photo_modal';
 import Modal from 'react-modal';
 
 class PhotoIndex extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      modalOpen: false
-    };
-  }
-
   componentDidMount() {
     this.props.fetchPhotos();
   }
@@ -36,7 +30,7 @@ class PhotoIndex extends React.Component {
           {photoItems}
         </div>
 
-      
+
       </div>
     );
   }
