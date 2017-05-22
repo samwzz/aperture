190651b1b93055bfaa3bb111e02dc33fee4785d6
Sam Wang <sam.zz.wang@gmail.com>
2017-05-22 15:52:45 -0700

    Add album index components

---
+++ frontend/components/album_index/album_index.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { withRouter, Route } from 'react-router-dom';
+import AlbumIndexItem from '../photo_index/album_index_item';
+
+class AlbumIndex extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numAlbums: null
+    };
+  }
+
+  componentDidMount() {
+    window.scrollTo(0, 0);
+    const { fetchPhotos, fetchUserPhotos, currentUser } = this.props;
+    if (this.props.location.pathname === `/users/${currentUser.id}`) {
+      fetchUserPhotos(currentUser.id)
+      .then(() => this.setState({
+        numPhotos: this.props.photos.length
+      }));
+      $('.cover-photo').parallax({imageSrc: 'https://res.cloudinary.com/db1ywnpgj/image/upload/v1495179973/pexels-photo-141635_ueizkw.jpg'});
+    } else {
+      fetchPhotos();
+    }
+  }
+
+  componentWillUnmount() {
+    $('.parallax-mirror').remove();
+  }
+
+  openModal() {
+    this.setState({ modalOpen: true });
+  }
+
+  closeModal() {
+    this.setState({ modalOpen: false });
+  }
+
+  userProfile() {
+    if (this.props.location.pathname === `/users/${this.props.currentUser.id}`) {
+      return(
+        <div className="profile-header-container">
+          <div className="cover-photo"></div>
+          <div className="header-spacer">
+            <div className="avatar">
+              <div className="avatar-wrapper">
+                <img src="https://res.cloudinary.com/db1ywnpgj/image/upload/v1495431600/Doge_hu9gbb.jpg"/>
+              </div>
+            </div>
+          </div>
+          <div className="user-nav">
+            <ul className="profile-tabs">
+              <li id="photostream-tab">
+                <a>Photostream</a>
+              </li>
+              <li id="album-tab">
+                <a>Albums</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  render () {
+    const { photos } = this.props;
+    const photoModals = photos.map((photo) => (
+      <PhotoModal className="photo-modal" key={`${photo.id}-user`} photo={photo} />
+    ));
+    let noPhotos;
+    if (this.state.numPhotos === 0) {
+      noPhotos = "You will find your photos here. Now, go out and take some photos!";
+    }
+
+    return (
+      <section className="photo-list-container">
+        {this.userProfile()}
+        <div className="photo-list">
+          <h2 className="no-photo-msg">{noPhotos}</h2>
+          {photoModals}
+        </div>
+      </section>
+    );
+  }
+}
+
+export default withRouter(PhotoList);
