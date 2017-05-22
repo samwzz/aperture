785c221072d3a62ddb174202eda2f5fc95a6195d
Sam Wang <sam.zz.wang@gmail.com>
2017-05-22 09:59:04 -0700

    Add associations for photos, users, albums

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,13 @@
 class Album < ApplicationRecord
   validates :title, :description, :user_id, presence: true
+
+  has_many :photos,
+    primary_key: :id,
+    foreign_key: :album_id,
+    class_name: :Photo
+
+  belongs_to :user,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :User
 end
