b989a8cc0011c2b58614f2185dcca6540c593390
Sam Wang <sam.zz.wang@gmail.com>
2017-05-27 13:55:13 -0700

    Remove tag validation

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,4 @@
 class Tag < ApplicationRecord
-  validates :name, uniqueness: { scope: [:taggable] }
+  # validates :name, uniqueness: { scope: [:taggable] }
   belongs_to :taggable, polymorphic: true
 end
