f27246029211b98a60a7b4f825556ea905b209cc
Sam Wang <sam.zz.wang@gmail.com>
2017-05-27 19:20:47 -0700

    Reset schema and put back tag seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170525223530) do
+ActiveRecord::Schema.define(version: 20170527195811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,11 +41,11 @@ ActiveRecord::Schema.define(version: 20170525223530) do
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.integer  "user_id",            null: false
+    t.string   "image_url"
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-    t.string   "image_url"
     t.index ["album_id"], name: "index_photos_on_album_id", using: :btree
     t.index ["user_id"], name: "index_photos_on_user_id", using: :btree
   end
