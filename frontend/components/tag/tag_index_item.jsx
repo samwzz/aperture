7aaa6c021c8057465f9b929c3e8a178132cfda19
Sam Wang <sam.zz.wang@gmail.com>
2017-05-28 18:20:49 -0700

    Deleting tags works

--- frontend/components/tag/tag_index_item.jsx
+++ frontend/components/tag/tag_index_item.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
 
-const TagIndexItem = ({ tag }) => (
+class TagIndexItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.deleteTag = this.deleteTag.bind(this);
+  }
 
-);
+  handleDelete(e) {
+    e.preventDefault();
+    this.props.deleteTag(this.props.tag);
+    delete this.props;
+  }
+
+  deleteTag() {
+    const { currentUser, photo } = this.props;
+    console.log(currentUser);
+    if (currentUser.id === photo.user_id) {
+      return(
+        <a className="delete-tag" onClick={ this.handleDelete }>
+          <i className="fa fa-times"></i>
+        </a>
+      );
+    }
+  }
+
+  render() {
+    const { tag } = this.props;
+
+    return(
+      <li key={`${tag.id}`} className="tag-item">
+          <div className="tag-item-wrapper">
+            <div>{tag.name}</div>
+            {this.deleteTag()}
+          </div>
+      </li>
+    );
+  }
+}
 
 export default TagIndexItem;
