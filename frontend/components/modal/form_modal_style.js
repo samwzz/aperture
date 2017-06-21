0fae34652ebe79b753d63201096bd74eabff40cc
Sam Wang <sam.zz.wang@gmail.com>
2017-06-20 22:51:27 -0700

    Lower opacity of form modal overlay

--- frontend/components/modal/form_modal_style.js
+++ frontend/components/modal/form_modal_style.js
@@ -5,7 +5,7 @@ const FormModalStyle = {
     left            : 0,
     right           : 0,
     bottom          : 0,
-    backgroundColor : 'rgba(0, 0, 0, 1)',
+    backgroundColor : 'rgba(0, 0, 0, 0.8)',
     zIndex          : 10
   },
   content : {
